37330d9553fc759cfb285f45b53b4fa874e93486
AB0623 <93128483+AB0623@users.noreply.github.com>
2022-01-09 11:03:11 +0800

    Update week-2.py

--- week-2/week-2.py
+++ week-2/week-2.py
@@ -54,18 +54,30 @@ maxProduct([-1,2])
 maxProduct([-1,0,2])
 maxProduct([-1,-2,0])
 
-#要求四
+#要求四-解法(1) 時間複雜度為O(n^2)
 def twoSum(nums,target):
     for i in range(len(nums)-1):
         n1=nums[i]
         for j in range(i+1,len(nums)):
             n2=nums[j]
             n3=n1+n2
-            result=[i,j]
             if n3 == target:
                 print("because "+"nums"+str([i])+"+"+"nums"+str([j])+" is "+str(n3))
+                result=[i,j]
                 return result
             j=j+1
 
+result=twoSum([2,11,7,15],9)
+print(result)
+
+#要求四-解法(2) 時間複雜度為O(n)
+def twoSum(nums,target):
+    for i in range(len(nums)):
+        n2=target-nums[i]
+        if (n2 in nums)==True:
+            j=nums.index(n2)
+            break
+    result=[i,j]
+    return result
 result=twoSum([2,11,7,15],9)
 print(result)
